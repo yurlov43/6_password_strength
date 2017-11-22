d0d24994040ae7bd2c212b406cf14588d0ed2064
yurlov43 <yurlov43@yandex.ru>
2017-11-22 16:34:27 +0300

    For a better understanding of the work of the program, comments are added in the symbol_groups_serch() and estimate_grouping()

--- password_strength.py
+++ password_strength.py
@@ -28,26 +28,35 @@ def estimate_password_length(
 
 
 def symbol_groups_serch(user_password, rating):
+    """Поиск в пароле символов различных групп"""
+    # поиск строчных букв
     if re.search(r"[a-z]+", user_password):
         rating += 1
+    # поиск заглавных букв
     if re.search(r"[A-Z]+", user_password):
         rating += 1
+    # поиск цифр
     if re.search(r"[0-9]+", user_password):
         rating += 1
+    # поиск спец символов, например, !@#$%^&*()-
     if re.search(r"[{}]+".format(punctuation), user_password):
         rating += 1
     return rating
 
 
 def estimate_grouping(user_password, rating):
-    password_pattern = re.compile(r"""(?P<lower_case>[a-z]+)
-        |(?P<uppercase>[A-Z]+)
-        |(?P<digits>[0-9]+)
-        |(?P<special_symbols>[{}]+)""".format(punctuation), re.X)
+    """Поиск в пароле групп различных символов. Например,
+    DSFdfdDSFsd - 4 группы чередующихся строчных и заглавных букв"""
+    password_pattern = re.compile(r"""
+        ([a-z]+)  #поиск строчных букв
+        |([A-Z]+) #поиск заглавных букв
+        |([0-9]+) #поиск цифр
+        |([{}]+)  #поиск спец символов, например, !@#$%^&*()-
+        """.format(punctuation), re.X)
     groups_in_password = password_pattern.finditer(user_password)
     groups_number = len(list(groups_in_password))
     if 0 < groups_number <= 2:
-        rating += 1 
+        rating += 1
     if 2 < groups_number <= 4:
         rating += 2
     if 4 < groups_number:
