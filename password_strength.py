2266e3671f8bdd5fd7bbbdff3f8cc6f4fe630933
yurlov43 <yurlov43@yandex.ru>
2017-11-23 19:30:46 +0300

    The estimate_grouping() function is removed because it duplicates the function symbol_groups_serch()

--- password_strength.py
+++ password_strength.py
@@ -28,38 +28,13 @@ def symbol_groups_serch(user_password, rating):
         rating += 1
     # поиск заглавных букв
     if re.search(r"[A-Z]+", user_password):
-        rating += 1
+        rating += 2
     # поиск цифр
     if re.search(r"[0-9]+", user_password):
-        rating += 1
+        rating += 2
     # поиск спец символов, например, !@#$%^&*()-
     if re.search(r"[{}]+".format(punctuation), user_password):
-        rating += 1
-    return rating
-
-
-def estimate_grouping(
-        user_password,
-        rating,
-        no_division_into_groups=0,
-        small_number_of_groups=2,
-        large_number_of_groups=4):
-    """Поиск в пароле групп различных символов. Например,
-    DSFdfdDSFsd - 4 группы чередующихся строчных и заглавных букв"""
-    password_pattern = re.compile(r"""
-        ([a-z]+)  #поиск группы строчных букв
-        |([A-Z]+) #поиск группы заглавных букв
-        |([0-9]+) #поиск группы цифр
-        |([{}]+)  #поиск группы спец. символов, например, !@#$%^&*()-
-        """.format(punctuation), re.X)
-    groups_in_password = password_pattern.finditer(user_password)
-    groups_number = len(list(groups_in_password))
-    if no_division_into_groups < groups_number <= small_number_of_groups:
-        rating += 1
-    if small_number_of_groups < groups_number <= large_number_of_groups:
         rating += 2
-    if large_number_of_groups < groups_number:
-        rating += 3
     return rating
 
 if __name__ == '__main__':
@@ -70,7 +45,6 @@ if __name__ == '__main__':
             rating = 1
             rating = estimate_password_length(user_password, rating)
             rating = symbol_groups_serch(user_password, rating)
-            rating = estimate_grouping(user_password, rating)
         else:
             sys.exit("Ошибка: введённый пароль не допустим.")
     else:
